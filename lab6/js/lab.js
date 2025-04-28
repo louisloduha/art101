985e8b0c0988f080cff40960aeb3ffd77a4e9fe9
louisloduha <lloduha@ucsc.edu>
2025-04-28 13:20:49 -0700

    Lab 6 work

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 - Arrays and Objects
+// Author: Louis Loduha
+// Date: 4/28/25
 
-// Constants
+// Define Transport Variables
+myTransport = [" Subaru Crosstrek", " Electric bike", " Walking", " Bus", " Rides from friends"]
 
-// Functions
+// Main Ride Object
+var myMainRide = {
+  make: "Subaru",
+  model: "Crosstrek",
+  color: "Black",
+  year: 2022,
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  // Here is a function to calculate the age of the Main Ride,
+  // It uses the current year minus the year of the car (this.year)
+  age: function() {
+      return 2025 - this.year;
+      }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Output
+document.writeln("The things I use to get around include:" + myTransport + "<br>");
 
-// let's get this party started
-main();
+document.writeln("Here is my main mode of transportation: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
