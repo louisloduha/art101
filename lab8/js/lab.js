400692cb00a0c72767a487c8df7219bc2bb6dfbc
louisloduha <lloduha@ucsc.edu>
2025-05-08 20:19:47 -0700

    lab 8 work

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -4,3 +4,19 @@
 
 // start code here
 
+coolArray = [7, 5, 22, 36, 99, 2, 4564]
+
+function isEven(num) {
+    return num % 2 === 0;
+  }
+
+console.log(coolArray.map(isEven))
+
+console.log(coolArray.map(function(x){
+    return x ** 0.5;
+}))
+
+
+
+
+
